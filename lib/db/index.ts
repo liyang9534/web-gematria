ea598224bb7b2093d4a85d9b2a44feb11dc46272
weijunext <weijunext@gmail.com>
2026-02-18 00:04:01 +0800

    chore: support DATABASE_URL is not set

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -2,11 +2,12 @@ import { createDatabase } from './config';
 
 const connectionString = process.env.DATABASE_URL;
 if (!connectionString) {
-  throw new Error('DATABASE_URL is not set');
+  // throw new Error('DATABASE_URL is not set');
+  console.log('DATABASE_URL is not set');
 }
 
 export const db = createDatabase({
-  connectionString,
+  connectionString: connectionString || '',
 });
 
 export const isDatabaseEnabled = !!connectionString;
